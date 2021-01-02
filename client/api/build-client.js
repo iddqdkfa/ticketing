da9f1d9dce2e9bbd8d934355e166acb32bb36953
iddqdkfa <aaronz@telus.net>
2021-01-02 00:43:38 -0800

    bug fixes

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ export default ({req}) => {
     if (typeof window ==='undefined'){
 
         return axios.create({
-            baseURL:'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL: 'http://www.ticketing-app-prd.xyz/',
             headers: req.headers
         })
     } else{
